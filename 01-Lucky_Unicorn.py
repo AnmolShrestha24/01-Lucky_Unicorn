9634b9588a24eed4c16c49d33dfa9a19ba8c5e27
AnmolShrestha24 <83674500+AnmolShrestha24@users.noreply.github.com>
2021-05-19 09:47:59 +1200

    yes/no checker v3

--- 01-Lucky_Unicorn.py
+++ 01-Lucky_Unicorn.py
@@ -1,9 +1,16 @@
-show_instructions = input ("Have you played this game before").lower()
-if show_instructions == "yes" or show_instructions == "y":
-    show_instructions = "yes"
-    print("program continues")
-elif show_instructions == "no" or show_instructions == "n":
-    show_instructions = "no"
-    print("display instructions")
-else:
-    print("Please answer yes / no")
+def yes_no(question)
+
+while show_instructions.lower() !="xxx"
+
+    show_instructions = input ("Have you played this game before").lower()
+
+        if show_instructions == "yes" or show_instructions == "y":
+        show_instructions = "yes"
+        print("program continues")
+
+        elif show_instructions == "no" or show_instructions == "n":
+        show_instructions = "no"
+        print("display instructions")
+
+        else:
+        print("Please answer yes / no")
